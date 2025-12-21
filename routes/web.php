e1b97ae134afb33228baddb3b02d9ebeb915f534
atheo810 <atheos810@gmail.com>
2025-12-21 21:03:45 +0800

    fix: typo

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/data', function () {
     return redirect()->route('data.index');
